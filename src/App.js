9f9c656c85756c728c7e5ffa677e212836137e8d
april <panapril77@gmail.com>
2021-04-17 00:08:34 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/ui/header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterGird from './components/characters/CharacterGird';
+import Search from './components/ui/Search';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      // console.log(result.data);;
+      const test = await axios(`https://foodish-api.herokuapp.com/api/images/biryani/`)
+      console.log(test)
+      setItems(result.data);;
+      setIsLoading(false);;
+    };
+    fetchItems();;
+
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          But sweet.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Search getQuery={(q)=>setQuery(q)} />
+      <CharacterGird isLoading={isLoading} items={items}  />
     </div>
   );
 }
---
+++ src/components/ui/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const header = () => {
+  return (
+    <header>
+      <img src={logo} alt='' />
+    </header>
+  );
+};
+
+export default header;
